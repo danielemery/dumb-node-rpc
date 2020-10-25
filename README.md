f9b78c7c0592050815bee0aa3a728ea9bc503d48
Daniel Emery <danielremery@gmail.com>
2020-10-25 14:10:41 +1030

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dumb Node RPC
+
+Experiment to generate boilerplate for typescript-typed http rpc calls between node microservices.
---
+++ samples/input/specification.types.ts
@@ -0,0 +1,6 @@
+export interface Widget {
+  name: string;
+}
+
+export interface GetWidgetsRequest {}
+export interface GetWidgetsResponse {}
