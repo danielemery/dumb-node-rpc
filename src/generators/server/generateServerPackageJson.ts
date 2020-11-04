1462f97f69f782e0c8a0141972d81f5abc683acf
Daniel Emery <danielremery@gmail.com>
2020-11-04 22:56:01 +1030

    Bumped base server version

--- src/generators/server/generateServerPackageJson.ts
+++ src/generators/server/generateServerPackageJson.ts
@@ -6,7 +6,7 @@ export default function packageJsonTemplate(
   const { author, dependencies = {}, license, name, version } = requirements;
   const allDependencies = [
     ...Object.entries(dependencies),
-    ['@danielemeryau/dumb-node-rpc-base-server', '0.1.0'],
+    ['@danielemeryau/dumb-node-rpc-base-server', '0.1.1'],
   ].sort((a, b) => a[0].localeCompare(b[0]));
   return `{
   "name": "${name}",
